bf96511714f1b775fe47c57e457476e581959000
Arian Bakh <arian@noklabs.ai>
2024-11-27 20:25:34 +0100

    Replace SQLite with TinyDB

--- judge/db/__init__.py
+++ judge/db/__init__.py
@@ -1,20 +1,8 @@
 import os
 from pathlib import Path
-from peewee import SqliteDatabase, Model, IntegerField, CharField
+from tinydb import TinyDB
 
 
 DATA_DIR = os.path.join(Path(__file__).parent.parent.parent, "data")
-DB_PATH = os.path.join(DATA_DIR, "books.db")
-DB = SqliteDatabase(DB_PATH)
-
-
-class Book(Model):
-    one_star = IntegerField()
-    two_star = IntegerField()
-    three_star = IntegerField()
-    four_star = IntegerField()
-    five_star = IntegerField()
-    image_path = CharField()
-
-    class Meta:
-        database = DB
+DB_PATH = os.path.join(DATA_DIR, "books.json")
+DB = TinyDB(DB_PATH)
--- populate_db.py
+++ populate_db.py
@@ -1,10 +1,26 @@
 import argparse
-from judge.db import DB, Book
+from judge.db import DB
+import os
+from tqdm import tqdm
 
 
 def populate(data_dir: str):
-    DB.connect()
-    DB.create_tables([Book])
+    for file_name in tqdm(os.listdir(data_dir)):
+        if file_name.endswith(".jpg"):
+            id = int(file_name.split("_")[0])
+            star1, star2, star3, star4, star5 = [
+                int(item) for item in file_name.split("_")[1].split(".")[0].split("-")
+            ]
+            image_path = os.path.join(data_dir, file_name)
+            DB.insert({
+                "id": id,
+                "star1": star1,
+                "star2": star2,
+                "star3": star3,
+                "star4": star4,
+                "star5": star5,
+                "image_path": image_path
+            })
 
 
 if __name__ == "__main__":
